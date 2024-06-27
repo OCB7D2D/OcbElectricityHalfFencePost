454e6ba419b310ad0b65b234c9fcba421ed3dc64
Marcel Greter <marcel.greter@ocbnet.ch>
2024-06-27 12:45:20 +0200

    Make shock relay tintable via tintmask shader

---
+++ Unity/HalfFencePost/Assets/Scripts/CachedMeshData.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: CachedMeshData
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+
+
+using System;
+using UnityEngine;
+
+[Serializable]
+public class CachedMeshData
+{
+  public string name;
+  public int vertexCount;
+  public int triCount;
+
+  public bool ApproximatelyEquals(CachedMeshData other) => this.name.Equals(other.name) && Mathf.Abs(this.vertexCount - other.vertexCount) < 10 && Mathf.Abs(this.triCount - other.triCount) < 10;
+}
